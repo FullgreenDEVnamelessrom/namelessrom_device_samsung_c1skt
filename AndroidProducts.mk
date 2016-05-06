NamelessRom/android_device_samsung_i9300

nameless bringup
Change-Id: I85ae3d9cb0e771499cbd969423ffaa3e055af413

AndroidProducts.mk
@@ -15,4 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_i9300.mk
+    $(LOCAL_DIR)/full_i9300.mk \
+    $(LOCAL_DIR)/nameless_i9300.mk

FullgreenDEVnamelessrom/namelessrom_device_samsung_c1skt

AndroidProducts.mk
@@ -15,4 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_c1skt.mk
+    $(LOCAL_DIR)/full_c1skt.mk \
+    $(LOCAL_DIR)/nameless_c1skt.mk